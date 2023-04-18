e7bff0f47416bb024ba39f5c968be9c1d97a0857
Koruto <flyingshadow03@gmail.com>
2023-04-18 17:17:20 +0530

    Creating Rooms and Adding Roles of the Channel

---
+++ constants.js
@@ -0,0 +1,6 @@
+export const ROOM_LIMIT = 3;
+export const PARENT_CATEGORY_ID = '881071692414345250';
+export const ROOM_NAME = ['Chane', 'Tane', 'Rain']; // Keep all the names in capitalized case, can also give space
+// To Do: Do this so first letter is Capital and if any space exist or not?, just keep them in proper
+export const DEFAULT_ROOM = 'Tane';
+// SOme problem with creating the default room, it should already exist, no?
